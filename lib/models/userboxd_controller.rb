1f464e37a120b99f07503a84cb2a2727a5b03448
Nat McConnaughay <natmcconnaughay@gmail.com>
2019-11-02 08:49:07 -0400

    controller work + structure

--- lib/models/userboxd_controller.rb
+++ lib/models/userboxd_controller.rb
@@ -2,18 +2,33 @@ class UserboxdController
 
     def start
         greeting
-        menu
-        #goodbye
+        menu_prompt
+        goodbye
     end
 
     def greeting
-        puts "Hello! Welcome to userboxd."
-        puts "(:-D)"
+        puts "-----------------------------------------"
+        puts "Hello! Welcome to userboxd. Here, you can \nget information on letterboxd users and \ncompare them!"
+        puts "-----------------------------------------"
+    end
+
+    def menu_prompt
+        puts "What do you want to do?"
+        puts "You can add a user, display your users," 
+        puts "display one user's info, compare two users,"
+        puts "or exit the program."
+        menu
     end
 
     def menu
-        
+        input = gets.strip
     end
+
+
+    def exit
+        puts "Goodbye!"
+    end
+
 
     
 
