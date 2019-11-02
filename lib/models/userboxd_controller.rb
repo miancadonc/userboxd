1ea0cf8ddfd246618ca49da0eb6aa276ef096f92
Nat McConnaughay <natmcconnaughay@gmail.com>
2019-11-02 09:07:36 -0400

    working on controller

--- lib/models/userboxd_controller.rb
+++ lib/models/userboxd_controller.rb
@@ -1,9 +1,11 @@
 class UserboxdController
 
+    VALID_INPUT = ['help', 'add user', 'display users', 'user info', 'compare users', 'exit']
+
     def start
         greeting
         menu_prompt
-        goodbye
+        exit
     end
 
     def greeting
@@ -17,17 +19,68 @@ class UserboxdController
         puts "You can add a user, display your users," 
         puts "display one user's info, compare two users,"
         puts "or exit the program."
-        menu
+        puts "You can also type help for help typing the commands."
+        puts "-----------------------------------------"
+        input
     end
 
-    def menu
-        input = gets.strip
+    def input
+        input = gets.strip.downcase
+        valid_input?(input)
     end
 
+    def valid_input?(input)
+        VALID_INPUT.include?(input) ? evalutate_input(input) : invalid_input
+    end
+
+    def evalutate_input(input)
+        #method = input.gsub(" ","_")
+
+        case input
+        when "exit"
+            exit
+        when "help"
+            help
+        when "add user"
+            add_user
+        when "display users"
+            display_users
+        when "user info"
+            user_info
+        when "compare users"
+            compare_users
+        end
+
+    end
+
+    def invalid_input
+    end
 
     def exit
         puts "Goodbye!"
     end
+
+    def help
+        puts "display help message"
+    end
+
+    def add_user
+        puts "add user"
+    end
+
+    def display_users
+        puts "display users"
+    end
+
+    def user_info
+        puts "user info"
+    end
+
+    def compare_users
+        puts "compare users"
+    end
+
+
 
 
     
