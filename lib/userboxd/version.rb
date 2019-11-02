8cc5429c9c96934aee6a1e45e451c32b2ccbd498
Nat McConnaughay <natmcconnaughay@gmail.com>
2019-11-02 06:46:06 -0400

    Initial commit. Bundler generated structure.

---
+++ lib/userboxd/version.rb
@@ -0,0 +1,3 @@
+module Userboxd
+  VERSION = "0.1.0"
+end
---
+++ spec/userboxd_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe Userboxd do
+  it "has a version number" do
+    expect(Userboxd::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
