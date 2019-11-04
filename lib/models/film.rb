940b6cde4398587f3b436a0b452c2855896919d8
Nat McConnaughay <natmcconnaughay@gmail.com>
2019-11-03 20:06:11 -0500

    extended and included modules on film class

--- lib/models/film.rb
+++ lib/models/film.rb
@@ -2,19 +2,6 @@ load 'lib/concerns/findable.rb'
 load 'lib/concerns/persistable.rb'
 class Film
     attr_accessor :name
-    @@all = []
-
-    def initialize(name)
-        @name = name
-    end
-
-    def self.create(name)
-        Film.new(name).save
-    end
-
-    def save
-        @@all << self
-    end
-
-
+    extend Persistable::ClassMethods, Findable
+    include Persistable::InstanceMethods, Nameable
 end
