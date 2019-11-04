71fd12bd72a855c9315d73af18e712129420224c
Nat McConnaughay <natmcconnaughay@gmail.com>
2019-11-03 19:42:14 -0500

    worked on "add users" functionality in controller

--- lib/models/userboxd_controller.rb
+++ lib/models/userboxd_controller.rb
@@ -1,6 +1,6 @@
 class UserboxdController
 
-    VALID_INPUT = ['help', 'add user', 'display users', 'user info', 'compare users', 'exit']
+    VALID_INPUT = ['help', 'add users', 'display users', 'user info', 'compare users', 'exit']
 
     def start
         User.create("mianc")
@@ -20,7 +20,7 @@ class UserboxdController
     end
 
     def menu_prompt
-        puts "You can add a user, display your users," 
+        puts "You can add users, display your users," 
         puts "display one user's info, compare two users,"
         puts "or exit the program."
         puts "You can also type help for help typing the commands."
@@ -30,6 +30,7 @@ class UserboxdController
     def input
         line
         puts "What would you like to do?"
+        line
         input = gets.strip.downcase
         valid_input?(input)
     end
@@ -46,6 +47,7 @@ class UserboxdController
     def invalid_input
         puts "I'm sorry, but I didn't understand what you wrote."
         puts "Would you like some help? (y/n)"
+        line
         help if get_answer == "y"
 
     end
@@ -57,6 +59,8 @@ class UserboxdController
         elsif answer == "n" || answer == "no"
             puts "Okay."
             input
+        elsif answer == "exit"
+            exit
         else
             invalid_input
         end
@@ -65,12 +69,14 @@ class UserboxdController
 
     def exit
         puts "Goodbye!"
+        line
     end
 
     def help
         puts "Here's a list of viable commands."
+        line
         puts "display users"
-        puts "add user"
+        puts "add users"
         puts "user info"
         puts "compare users"
         puts "help"
@@ -78,25 +84,37 @@ class UserboxdController
         input
     end
 
-    def add_user #NEEDS BETTER OPTIONS TO ADD ANOTHER USER
+    def get_username
+        line
         puts "Please enter a letterboxd username:"
         username = gets.strip
+        username == "done" ? input : add_user(username)
+    end
+
+    def add_user(username)
         if User.exists?(username) 
             puts "Youv'e already added that user!"
-            line
-            puts "Would you like to add another user? (y/n)"
-            add_user if get_answer == "y"
-        # elsif User.new(username).valid? == false
+            get_username
+
+            # elsif User.new(username).valid? == false
         #     puts "Looks like that user doesn't exit!"
         #     puts "Would you like to add another user? (y/n)"
         #     add_user if get_answer == "y"
         else 
             User.create(username)
             puts "#{username} added."
-            line
-            puts "Would you like to add another user? (y/n)"
-            add_user if get_answer == "y"
+            get_username
         end
+    end
+
+
+
+    def add_users #NEEDS BETTER OPTIONS TO ADD ANOTHER USER
+        line
+        puts "Add as many users as you want, adding them one at a time."
+        puts "When you're done, type \"done\""
+        get_username
+        
     end
 
     def display_users
